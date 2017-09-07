a6055bd2581d5e87cb83cf5bd38bc269cb823b4e
Janice Wong <go.janicewong@gmail.com>
2017-09-07 17:22:06 -0400

    Update editable location and job type to dropdown and radio

--- app/assets/javascripts/components/_job.js.jsx
+++ app/assets/javascripts/components/_job.js.jsx
@@ -1,6 +1,6 @@
 var Job = React.createClass({
   getInitialState() {
-    return {editable: false, location: this.props.job.location}
+    return {editable: false, jobtype: this.props.job.jobtype}
   },
 
   handleEdit() {
@@ -9,18 +9,23 @@ var Job = React.createClass({
       var title = this.refs.title.value;
       var description = this.refs.description.value;
       var location = this.refs.location.value;
-      var jobtype = this.refs.jobtype.value;
+      var jobtype = this.state.jobtype;
       var job = {id: id, title: title, description: description, location: location, jobtype: jobtype }
       this.props.handleUpdate(job);
     }
     this.setState({editable: !this.state.editable})
   },
 
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  },
+
   render() {
     var title = this.state.editable ? <p>Title: <input type='text' ref='title' defaultValue={this.props.job.title} /></p> : <h3> {this.props.job.title}</h3>;
+
     var location = this.state.editable ? 
       <p>Location:
-        <select ref="location" defaultValue={this.state.location} >
+        <select ref="location" defaultValue={this.props.job.location} >
           <option value="New York">New York</option>
           <option value="Los Angeles">Los Angeles</option>
           <option value="San Francisco">San Francisco</option>
@@ -29,8 +34,29 @@ var Job = React.createClass({
       </p> 
       : 
       <p><strong>Location:</strong> {this.props.job.location}</p>;
-    var jobtype = this.state.editable ? <p>Type: <input type='text' ref='jobtype' defaultValue={this.props.job.jobtype} /></p> : <p><strong>Type:</strong> {this.props.job.jobtype}</p>;
+
+    var jobtype = this.state.editable ? 
+      <p>Type:
+          <br />
+          <label>
+            <input name="jobtype" ref="jobtype" type="radio" value="Full-time" 
+                          checked={this.state.jobtype === 'Full-time'} 
+                          onChange={this.handleChange} />
+            Full-time
+          </label>
+          <br />
+          <label>
+            <input name="jobtype" ref="jobtype" type="radio" value="Part-time" 
+                          checked={this.state.jobtype === 'Part-time'} 
+                          onChange={this.handleChange} />
+            Part-time
+          </label>
+      </p> 
+      : 
+      <p><strong>Type:</strong> {this.props.job.jobtype}</p>;
+
     var description = this.state.editable ? <p>Description: <textarea type='text' ref='description' defaultValue={this.props.job.description}></textarea></p> : <p><i>{this.props.job.description}</i></p>;
+
     return (
       <div>
         {title}
